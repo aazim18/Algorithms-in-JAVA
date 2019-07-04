821ce42f66db6c04b39cf0f9dc120eb1d63788d3
aazim18 <45408191+aazim18@users.noreply.github.com>
2019-07-04 08:22:33 -0400

    Add files via upload

---
+++ AVLtree/Tree.java
@@ -0,0 +1,20 @@
+package avl;
+
+
+public interface Tree<T extends Comparable<T>> {
+    /**
+     * @param data The data to insert
+     * */
+    void insert(T data);
+    /**
+     * @param dataToRemove
+     * */
+    void remove(T dataToRemove);
+
+    /**
+     * traverse the tree.
+     * */
+    void traverse();
+
+    boolean isEmpty();
+}
